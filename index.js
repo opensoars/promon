0ba0cadfda38ec5542707ff7b338b005dd5e0114
unknown <sam_1700@hotmail.com>
2015-06-26 13:46:07 +0200

    Typo

--- index.js
+++ index.js
@@ -4,10 +4,16 @@
  */
 var app = {};
 
+app.require = require;
+
 /**
  * Application modules namespace.
  * @namespace
  */
 app.modules = require('lib/util/moduleLoader')({
   Ezlog: 'ezlog'
+});
+
+app.modules = require('lib/util/moduleLoader')(app)({
+
 });
