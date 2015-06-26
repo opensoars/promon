132d17a56136629bf3309f87a9cf8936b99cae07
unknown <sam_1700@hotmail.com>
2015-06-26 17:03:00 +0200

    Root __dirname bound to application namespace

--- index.js
+++ index.js
@@ -4,11 +4,22 @@
  */
 var app = {};
 
+
+/** Application configuration read from config.json */
+app.config = require('./config.json');
+
+
 /** Bind root require function to application namespace. */
 app.require = require;
 
+
+/** Bind application root directory to application namespace */
+app.__dirname = __dirname;
+
+
 /** Utility function that loads key value mapped modules. */
 app.moduleLoader = require('./lib/util/moduleLoader')(app);
+
 
 /**
  * Application modules namespace.
