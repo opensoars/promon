24b21e2daf0e29cbdf7bdfa0e4b7a6ecd9bf72d0
unknown <sam_1700@hotmail.com>
2015-06-26 13:28:20 +0200

    Added basic module loader boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(process.argv);
+/**
+ * Top level application namespace.
+ * @namespace
+ */
+var app = {};
+
+/**
+ * Application modules namespace.
+ * @namespace
+ */
+app.modules = require('lib/util/moduleLoader')({
+  Ezlog: 'ezlog'
+});
